46fcf151a0a8fd5a484717a455b9b1ee687e0dcf
[MustafainHaidery] <[mmustafainhaidery@gmail.com]>
2024-12-07 09:13:36 +0500

    Assignment Completed

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="w-full h-[60vh] flex flex-col justify-center items-center px-10 md:px-0">
+      <h2 className="text-5xl text-center md:text-6xl text-transparent bg-gradient-to-b from-gray-800 to-gray-600 bg-clip-text font-bold">Sample Page</h2>
+      <p className="text-gray-500 text-center">Sample Web Page</p>
+    </div>
+  );
+}
